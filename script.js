ec0d8ef8ebe5394f8acb6b30af8c9e1bbdd7bc0d
thigo47 <vilaportilho@gmail.com>
2023-04-18 20:42:25 +0100

    att

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let botao = document.getElementById("botao")
+let container = document.getElementById("conteiner")
 
 botao.addEventListener("click" , function(){
     let tweetescrito = document.getElementById("tweetescrito")
     let tweetpostado = document.getElementById("tweetpostado")
 
-tweetpostado.innerHTML = tweetescrito.value + "<br>"
+    if (tweetescrito.value.length == 0) {
+        window.alert("escreva primeiro")
+    } else {
+        for (const c in tweetescrito) {
+            tweetpostado.innerHTML = tweetescrito.value
+        }
+    }
+
+})
+
+let tema = document.getElementById("tema")
+
+tema.addEventListener("click", function () {
+
+    if (document.body.style.backgroundColor =="black") {
+
+        document.body.style.backgroundColor="white"
+        
+    } else {
+        document.body.style.backgroundColor="black"
+    }
+   
 })
